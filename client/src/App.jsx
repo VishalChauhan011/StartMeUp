a8d2d9d786d1a5f14a05bb78ed059a9b627d5e31
VishalChauhan011 <vishalchauhan46750@gmail.com>
2023-04-03 22:41:45 +0530

    Add Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return <div className="text-center text-blue-900 text-8xl">APP</div>;
 };
 
 export default App;
