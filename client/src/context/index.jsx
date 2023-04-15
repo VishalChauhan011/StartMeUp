84b2de98cbf0a1172bd46db0fdbc86946a7f4f3b
VishalChauhan011 <vishalchauhan46750@gmail.com>
2023-04-15 09:41:50 +0530

    Complete CampaignDetails page

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -68,7 +68,8 @@ export const StateContextProvider = ({ children }) => {
   };
 
   const donate = async (pId, amount) => {
-    const data = await contract.call("donateToCampaign", pId, {
+    console.log(amount);
+    const data = await contract.call("donateToCampaign", [pId], {
       value: ethers.utils.parseEther(amount),
     });
 
@@ -76,7 +77,7 @@ export const StateContextProvider = ({ children }) => {
   };
 
   const getDonations = async (pId) => {
-    const donations = await contract.call("getDonators", pId);
+    const donations = await contract.call("getDonators", [pId]);
     const numberOfDonations = donations[0].length;
 
     const parsedDonations = [];
